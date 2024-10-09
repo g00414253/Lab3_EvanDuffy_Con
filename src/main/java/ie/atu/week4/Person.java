8031ed92ef94f11b0a2c82e6a1f0b9c4d81033c7
G00414253@atu.ie <g00414253@atu.ie>
2024-10-09 15:08:55 +0100

    Finished adding validation to all the arguments needed

--- src/main/java/ie/atu/week4/Person.java
+++ src/main/java/ie/atu/week4/Person.java
@@ -1,5 +1,6 @@
 package ie.atu.week4;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
@@ -12,7 +13,41 @@ import lombok.NoArgsConstructor;
 public class Person {
     @NotBlank(message = "You must enter a name")
     private String name;
-    @Min(value=1,message="Must be greater or equal to 1")
-    private int id;
+    @NotBlank(message = "You must enter a title")
+    private String title;
+    @Min(value=1,message = "Must be greater or equal to one")
+    private int employeeId;
+    @Min(value=1,message = "Must be greater or equal to one")
+    private int age;
+    @Email(message = "You must enter a vaild email")
+    private String email;
+    @NotBlank(message = "You must enter a position")
+    private String position;
+    @NotBlank(message = "You must enter a department")
+    private String department;
 
 }
+
+/*
+Talend ID Block to test it working with valid arguments
+{
+        "name" : "evan",
+        "title" : "student",
+        "employeeId" : 7,
+        "age" : 21,
+        "email" : "evan@email.com",
+        "position" : "student",
+        "department" : "electronics"
+        }
+
+Talend ID block to test teh validation is working when there is errors in the arguments
+{
+  "name" : "",
+  "title" : "",
+  "employeeId" : 0,
+  "age" : 0,
+  "email" : "evanemail.com",
+  "position" : "",
+  "department" : ""
+  }
+ */
